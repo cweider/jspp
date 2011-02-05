64acd6d254ec95fa72173a3e746e368fe7021b1e
Chad Weider <cweider@oofn.net>
2011-02-05 00:58:30 -0800

    Improved ouput.

--- tests/out.js
+++ tests/out.js
@@ -1,40 +1,23 @@
-var __MODULES__ = __MODULES__ || {};
-var require = (function () {
-    var _require = function (absolutePath) {
-        var module;
-        var suffixes = ['', '.js', '/index.js'];
-        for (var i = 0, ii = suffixes.length; i < ii && !module; i++) {
-            var suffix = suffixes[i];
-            var _path = absolutePath + suffix;
-            module = __MODULES__[_path];
-        }
+var require = (typeof require != 'undefined') && require.install ? require : (function () {
+    var modules = {};
+    var main = null;
 
-        if (!module) {
-	        throw new Error("The module at \"" + absolutePath + "\" does not exist.");
-        }
-
-        // If it's a function then it hasn't been exported yet. Run function and
-        //  then replace with exports result.
-        if (module instanceof Function) {
-            var _exports = {};
-            var _module = {id: absolutePath};
-            module.call(_module, _requireRelativeTo(absolutePath.replace(/[^\/]+$/,'')), _exports, _module);
-            module = _exports;
-            __MODULES__[absolutePath] = module;
-        }
-
-        return module;
-    };
     var normalizePath = function (path) {
-        var isAbsolute = path.charAt(0) == '/';
         var pathComponents1 = path.split('/');
         var pathComponents2 = [];
+        if (path.charAt(0) == '/') {
+            pathComponents1.unshift('');
+        }
+
         var component;
         for (var i = 0, ii = pathComponents1.length; i < ii; i++) {
             component = pathComponents1[i];
             switch (component) {
                 case '':
                 case '.':
+                    if (i == 0) {
+                        pathComponents2.push(component);
+                    }
                     break;
                 case '..':
                     if (pathComponents2.length) {
@@ -46,77 +29,122 @@ var require = (function () {
             }
         }
 
-        return (isAbsolute ? '/' : '') + pathComponents2.join('/');
+        return pathComponents2.join('/');
     };
-    var resolvePath = function (relativePath, path) {
-        var absolutePath;
-        if (path.charAt(0) == '/') {
-            absolutePath = path;
-        } else {
-            absolutePath = relativePath + '/' + path;
+
+    var rootedPath = function (path, relativePath) {
+        var topLevelPath = path;
+        if (path.charAt(0) == '.' && (path.charAt(1) == '/' || (path.charAt(1) == '.' && path.charAt(2) == '/'))) {
+             topLevelPath = (relativePath || '/') + path;
         }
-        return normalizePath(absolutePath);
+        return topLevelPath;
     };
-    var _requireRelativeTo = function (relativePath) {
-      return function (path) {
-        var absolutePath = resolvePath(relativePath, path);
-        return _require(absolutePath);
-      };
-    };
-    return _requireRelativeTo('/');
-}());
 
-var declareModule = function (absolutePath, module) {
-    if (!Object.prototype.hasOwnProperty.call(__MODULES__, absolutePath)) {
-        __MODULES__[absolutePath] = module;
-    } else {
-        // Drop import silently.
-    }
-};
-
-var declareModules = function (pathModuleMap) {
-    for (var absolutePath in pathModuleMap) {
-        if (Object.prototype.hasOwnProperty.call(pathModuleMap, absolutePath)) {
-            declareModule(absolutePath, pathModuleMap[absolutePath]);
+    var moduleAtPath = function (topLevelPath) {
+        var suffixes = ['', '.js', '/index.js'];
+        for (var i = 0, ii = suffixes.length; i < ii; i++) {
+            var suffix = suffixes[i];
+            var path = topLevelPath + suffix;
+            var module = Object.prototype.hasOwnProperty.call(modules, path) && modules[path];
+            if (module) {
+                // If it's a function then it hasn't been exported yet. Run function and
+                //  then replace with exports result.
+                if (module instanceof Function) {
+                    var _module = {id: topLevelPath, exports: {}};
+                    if (!main) {
+                        main = _module;
+                    }
+                    modules[topLevelPath] = _module;
+                    module(requireRelativeTo(path.replace(/[^\/]+$/,'')), _module.exports, _module);
+                    module = _module;
+                }
+                return module;
+            }
         }
-    }
-};
+        return null;
+    };
 
-declareModules({
+    var requireRelativeTo = function (relativePath) {
+        var _require = function (path) {
+            var topLevelPath = normalizePath(rootedPath(path, relativePath));
+            var module = moduleAtPath(topLevelPath);
+            if (!module) {
+                throw new Error("The module at \"" + topLevelPath + "\" does not exist.");
+            }
+            return module.exports;
+        };
+        var _install = function (topLevelPath, module) {
+            if (typeof topLevelPath != 'string' || typeof module != 'function') {
+                throw new Error("Argument error: install must be given a string function pair.");
+            }
+
+            if (moduleAtPath(topLevelPath)) {
+                // Drop import silently
+            } else {
+                modules[topLevelPath] = module;
+            }
+        };
+        _require.install = function (topLevelPathOrModuleMap, module) {
+            if (typeof topLevelPathOrModuleMap == 'object') {
+                var moduleMap = topLevelPathOrModuleMap;
+                for (var topLevelPath in moduleMap) {
+                    if (Object.prototype.hasOwnProperty.call(moduleMap, topLevelPath)) {
+                        _install(topLevelPath, moduleMap[topLevelPath]);
+                    }
+                }
+            } else {
+                _install(topLevelPathOrModuleMap, module);
+            }
+        };
+        _require._modules = modules;
+        _require.main = main;
+
+        return _require;
+    };
+
+    return requireRelativeTo('/');
+})();
+
+require.install({
   "/root.js": function (require, exports, module) {
     var Model = require("/lib/model");
     var Button = require("/lib/ui/button");
     var Util = require("/lib/util/util");
     var SpecialButton = require("/lib/ui/ext/special_button");
-    
+
     console.log('Model is:');
     console.dir(Model);
-    
+
     console.log('Button is:');
     console.dir(Button);
-    
+
     console.log('SpecialButton is:');
     console.dir(SpecialButton);
-    
+
     console.log('Util is:');
     console.dir(Util);
     },
   "/lib/model/index.js": function (require, exports, module) {
-    exports.getSomeData = function () {};
-    exports.setSomeData = function () {};
+    var square = require("./square");
+    var circle = require("./circle");
+    var triangle = require("./triangle");
+
+    exports.square = require('./square');
+    exports.circle = require('./circle');
+    exports.triangle = require('./triangle');
     },
   "/lib/ui/button.js": function (require, exports, module) {
     var Control = require("./control");
     var Util = require("../util/util");
     console.log('MODULE RUN: BUTTON');
-    
+
     exports.button1 = 'button1';
     exports.button2 = 'button2';
     exports.Control = Control;
     },
   "/lib/util/util.js": function (require, exports, module) {
     console.log('MODULE RUN: UTIL');
-    
+
     exports.escapeHTML = function () {};
     exports.escapeHTMLAttribute = function () {};
     exports.importantURL = 'http://example.com/';
@@ -126,17 +154,27 @@ declareModules({
     var Button = require("../../ui/button");
     var Util = require("/lib/util/util");
     console.log('MODULE RUN: SPECIAL BUTTON');
-    
+
     exports.Button = Button;
     exports.Control = Control;
     exports.Util = Util;
     },
+  "/lib/model/square.js": function (require, exports, module) {
+    exports.type = "SQUARE";
+    },
+  "/lib/model/circle.js": function (require, exports, module) {
+    exports.type = "CIRCLE";
+    },
+  "/lib/model/triangle.js": function (require, exports, module) {
+    exports.type = "TRIANGLE";
+    },
   "/lib/ui/control.js": function (require, exports, module) {
     var Util = require("../util/util");
     console.log('MODULE RUN: CONTROL');
-    
+
     exports.i_am_a_control = true;
     exports.utils = Util;
     }
 });
+require("/root")
 
